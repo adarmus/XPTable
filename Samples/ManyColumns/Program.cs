8be5ced3ccbb765b59ce2684d1c148d8cfd3b132
adarmus <adam@blairfamily.co.uk>
2017-02-15 12:15:30 +0000

    Dummy comment

--- Samples/ManyColumns/Program.cs
+++ Samples/ManyColumns/Program.cs
@@ -9,7 +9,7 @@ namespace DemoApplication
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// The main entry point for the application. TEST
         /// </summary>
         [STAThread]
         static void Main()
